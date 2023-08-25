7f0d0104bec85e7acff0d2585a882aa60b3569ae
jauumgod <joaofs901@gmail.com>
2023-08-25 06:51:44 -0300

    feat: create appconfig

---
+++ src/main/java/com/picpaysimplificado/endpoints/Endpoint.java
@@ -0,0 +1,7 @@
+package com.picpaysimplificado.endpoints;
+
+public class Endpoint {
+
+    public static String apiURL = "https://run.mocky.io/v3/8fafdd68-a090-496f-8c9a-3442cf30dae6";
+    public static String mocLabNotify = "http://o4d9z.mocklab.io/notify";
+}
