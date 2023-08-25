fd15a3d94b6cf5a7d341ae44b40a7dd85aeb2539
jauumgod <joaofs901@gmail.com>
2023-08-25 06:49:51 -0300

    feat: update User.java

--- src/main/java/com/picpaysimplificado/domain/user/User.java
+++ src/main/java/com/picpaysimplificado/domain/user/User.java
@@ -1,11 +1,9 @@
 package com.picpaysimplificado.domain.user;
 
 
+import com.picpaysimplificado.dtos.UserDTO;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.math.BigDecimal;
 
@@ -14,6 +12,7 @@ import java.math.BigDecimal;
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 @EqualsAndHashCode(of="id")
 
 public class User {
@@ -22,18 +21,25 @@ public class User {
     private Long id;
     private String firstName;
     private String lastName;
-
     @Column(unique = true)
     private String document;
-
     @Column(unique = true)
     private String email;
-
     @Column(unique = true)
     private String password;
     private BigDecimal balance;
 
     @Enumerated(EnumType.STRING)
-    private UserType userType;
+    public UserType userType;
+
+    public User(UserDTO data){
+        this.firstName = data.firstName();
+        this.lastName = data.lastName();
+        this.document = data.document();
+        this.email = data.email();
+        this.password = data.password();
+        this.balance = data.balance();
+        this.userType = data.userType();
+    }
 
 }
